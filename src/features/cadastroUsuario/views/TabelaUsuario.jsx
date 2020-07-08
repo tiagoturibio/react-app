80e30eb2de4aa71dfbcaad4f0d6a5870adf07c9a
Tiago Turibio <tiago.turibio@dtidigital.com.br>
2020-07-08 19:52:09 -0300

    Refatoracao da tabela de usuario

--- src/features/cadastroUsuario/views/TabelaUsuario.jsx
+++ src/features/cadastroUsuario/views/TabelaUsuario.jsx
@@ -1,26 +1,14 @@
 import React from "react";
 import { Table, TableContainer, Paper } from "@material-ui/core";
-import TabelaHeader from "./TabelaHeader";
 import PropTypes from "prop-types";
-import LinhaUsuario from "./LinhaUsuario";
-import TabelaBody from "./TabelaBody";
 
 const TabelaUsuario = (props) => {
-  const { data, ativarUsuario } = props;
-  const colunas = [
-    { alinhamento: "center", nome: "ID" },
-    { alinhamento: "center", nome: "Nome" },
-    { alinhamento: "center", nome: "Permissão" },
-    { alinhamento: "center", nome: "Ativo" },
-  ];
+  const { templateBody, templateHeader } = props;
   return (
     <TableContainer component={Paper}>
       <Table aria-label="simple table">
-        <TabelaHeader colunas={colunas} />
-        <TabelaBody
-          data={data}
-          render={(usuario) => <LinhaUsuario key={usuario.id} usuario={usuario} ativarUsuario={ativarUsuario} />}
-        />
+        {templateHeader}
+        {templateBody}
       </Table>
     </TableContainer>
   );
--- src/features/cadastroUsuario/views/UsuarioPage.jsx
+++ src/features/cadastroUsuario/views/UsuarioPage.jsx
@@ -1,15 +1,34 @@
 import React, { useEffect } from "react";
 import { Grid } from "@material-ui/core";
 import TabelaUsuario from "./TabelaUsuario";
+import TabelaBody from "./TabelaBody";
+import LinhaUsuario from "./LinhaUsuario";
+import TabelaHeader from "./TabelaHeader";
 
 const UsuarioPage = (props) => {
   const { usuarios, listarUsuarios, ativarUsuario } = props;
+  const colunas = [
+    { alinhamento: "center", nome: "ID" },
+    { alinhamento: "center", nome: "Nome" },
+    { alinhamento: "center", nome: "Permissão" },
+    { alinhamento: "center", nome: "Ativo" },
+  ];
   useEffect(() => {
     listarUsuarios();
   }, [listarUsuarios]);
   return (
     <Grid container className="App" justify="center" direction="row">
-      <TabelaUsuario data={usuarios} ativarUsuario={ativarUsuario} />
+      <TabelaUsuario
+        data={usuarios}
+        ativarUsuario={ativarUsuario}
+        templateBody={
+          <TabelaBody
+            data={usuarios}
+            render={(usuario) => <LinhaUsuario key={usuario.id} usuario={usuario} ativarUsuario={ativarUsuario} />}
+          />
+        }
+        templateHeader={<TabelaHeader colunas={colunas} />}
+      />
     </Grid>
   );
 };
